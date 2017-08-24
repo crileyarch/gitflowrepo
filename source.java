c5462ec141f7e1b9dfd7f449c951c94f5bde92dc
Chris Riley <chris.riley@cprime.com>
2017-08-24 13:44:46 -0400

    US-1024 Added transformation support via new transformer and udpated of main.

--- source.java
+++ source.java
@@ -3,4 +3,6 @@ package com.example
 public void main(String args[]){
 
 System.out.println("Batch processing at JeffNat");
+
+System.out.println("Transaction support for YAML.");
 }
---
+++ transform.java
@@ -0,0 +1,10 @@
+package com.example
+
+public void main(String args[]){
+
+System.out.println("Transformation logic here.");
+
+System.out.printlng("Done now.");
+
+
+}
